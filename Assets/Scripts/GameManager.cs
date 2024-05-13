709d8ba1034cf442172880baf76086f366fe5001
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-13 13:02:48 +0530

    Ready to Play!!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,48 @@
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameover;
     public int score;
 
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        pause();
+    }
+    public void play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+        playButton.SetActive(false);
+        gameover.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for(int i = 0; i< pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+    public void pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
     public void GameOver()
     {
-        Debug.Log("GAME OVER");
+        gameover.SetActive(true);
+        playButton.SetActive(true);
+        pause();
     }
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }
